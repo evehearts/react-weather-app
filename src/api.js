9f6c9f0d940a5beff77fa3ff41cab24f207a20bf
evehearts <evehfreitas@outlook.com>
2022-08-04 17:24:50 -0300

    react-weather-commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b43767da3amsh37ebd009c572322p1e7477jsn2f107a614c08',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0425b127c5d825c16c97d07d97d1ff5e";
